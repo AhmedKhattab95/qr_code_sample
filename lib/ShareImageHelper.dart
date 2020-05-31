128cffa99aa162aa892bb865ba6d09688bb9c961
ahnmed khattab <akhattab@facegraph.com>
2020-05-31 22:23:00 +0200

    intial commit

---
+++ lib/ShareImageHelper.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:esys_flutter_share/esys_flutter_share.dart';
+
+/// class that will take image url try to download it after that will preview share
+class ShareImageHelper {
+  ////////// singleton
+  ShareImageHelper._privateConstructor();
+
+  static ShareImageHelper _instance;
+
+  static ShareImageHelper get instance {
+    if (_instance == null) {
+      _instance = ShareImageHelper._privateConstructor();
+    }
+    return _instance;
+  }
+
+  ////////// actions
+  Future shareImage(Uint8List imageAsUint8List) async{
+    await Share.files(
+        "share image",
+        {
+          'image.png': imageAsUint8List,
+        },
+        '*/*');
+
+  }
+
+
+
+}
